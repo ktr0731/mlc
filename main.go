ac48cc471e394b9eaf93b3e7c23f86abf0a584b4
Taro Aoki <aizu.s1230022@gmail.com>
2017-02-20 22:10:05 +0900

    [MOD] Refactored main.go

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"os/exec"
 	"os/signal"
@@ -31,12 +32,12 @@ func main() {
 	for _, cmd := range cmds {
 		out, err := cmd.StdoutPipe()
 		if err != nil {
-			// log.Println("cannot get stdout")
+			logging("cannot get stdout")
 			return
 		}
 		errp, err := cmd.StderrPipe()
 		if err != nil {
-			// log.Println("cannot get stderr")
+			logging("cannot get stderr")
 			return
 		}
 
@@ -44,62 +45,61 @@ func main() {
 		go scan(wg, errp, os.Stderr)
 
 		if err := cmd.Start(); err != nil {
-			// log.Printf("error while starting child processes: %s", err)
+			logging(fmt.Sprintf("error while starting child processes: %s", err))
 			// TODO: 起動してるプロセスを閉じる
 			return
 		}
-		// log.Println("PID:", cmd.Process.Pid)
-		cmd.Stdout = os.Stdout
 	}
 
 	exitChan := make(chan struct{}, 1)
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, os.Interrupt)
 
-	// log.Println("Start")
+	logging("Start")
 	go func() {
 		<-sig
-		// log.Println("Interrupt")
-		// log.Println("Send signal to children")
+
+		logging("Interrupt")
+		logging("Send interrupt signal to children")
 
 		for _, cmd := range cmds {
 			if err := cmd.Process.Signal(os.Interrupt); err != nil {
-				// log.Printf("error while killing child processes: %s", err)
+				logging(fmt.Sprintf("error while killing child processes: %s", err))
 				return
 			}
 
 			_, err := cmd.Process.Wait()
 			if err != nil {
-				// log.Printf("error while waiting child process: %s", err)
+				logging(fmt.Sprintf("error while waiting child process: %s", err))
 			}
-
-			// log.Println("exit on ", stat.String())
 		}
 
-		// log.Println("process killed")
+		logging("childlen processes killed")
 
 		exitChan <- struct{}{}
 	}()
 
 	go func() {
 		wg.Wait()
-		// log.Println("processes finished")
+		logging("all processes terminated")
 		exitChan <- struct{}{}
 	}()
 
 	<-exitChan
 
-	// log.Println("Stop graceful")
+	logging("Stop")
 }
 
 func scan(wg *sync.WaitGroup, reader io.Reader, out io.Writer) {
-	defer func() {
-		// log.Println("close scan")
-	}()
-	// log.Println("create scanner")
 	s := bufio.NewScanner(reader)
 	for s.Scan() {
 		fmt.Fprintln(out, s.Text())
 	}
 	wg.Done()
+}
+
+func logging(message string) {
+	if debug := os.Getenv("DEBUG"); debug != "" {
+		log.Println(message)
+	}
 }
