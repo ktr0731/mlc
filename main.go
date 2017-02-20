f5255f2cd8c78a1a3752a61f89213ccb26758a04
Taro Aoki <aizu.s1230022@gmail.com>
2017-02-20 20:24:04 +0900

    [MOD] Add two to WaitGroup because for stdout and stderr

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"io"
-	"log"
 	"os"
 	"os/exec"
 	"os/signal"
@@ -12,28 +11,17 @@ import (
 	"sync"
 )
 
-func scan(wg *sync.WaitGroup, reader io.Reader, out io.Writer) {
-	defer func() {
-		log.Println("close scan")
-	}()
-	log.Println("create scanner")
-	s := bufio.NewScanner(reader)
-	for s.Scan() {
-		fmt.Fprintln(out, s.Text())
-	}
-	wg.Done()
-}
-
 func main() {
 	args := os.Args
 	if len(args) == 1 {
-		log.Fatal("invalid argument")
+		fmt.Fprintln(os.Stderr, "invalid argument")
+		return
 	}
 
 	wg := new(sync.WaitGroup)
 	cmds := make([]*exec.Cmd, len(args[1:]))
 	for i, arg := range args[1:] {
-		wg.Add(1)
+		wg.Add(2) // stdout and stderr
 		cmdArgs := strings.Split(arg, " ")
 		cmds[i] = exec.Command("sh", append([]string{"-c"}, strings.Join(cmdArgs, " "))...)
 	}
@@ -41,25 +29,26 @@ func main() {
 	// TODO: go-shellwords を使う
 
 	for _, cmd := range cmds {
-		// TODO: goroutineつかう？
 		out, err := cmd.StdoutPipe()
 		if err != nil {
-			log.Println("cannot get stdout")
+			// log.Println("cannot get stdout")
 			return
 		}
-		// errp, err := cmd.StderrPipe()
-		// if err != nil {
-		// 	log.Println("cannot get stderr")
-		// 	return
-		// }
+		errp, err := cmd.StderrPipe()
+		if err != nil {
+			// log.Println("cannot get stderr")
+			return
+		}
+
 		go scan(wg, out, os.Stdout)
+		go scan(wg, errp, os.Stderr)
 
 		if err := cmd.Start(); err != nil {
-			log.Printf("error while starting child processes: %s", err)
+			// log.Printf("error while starting child processes: %s", err)
 			// TODO: 起動してるプロセスを閉じる
 			return
 		}
-		log.Println("PID:", cmd.Process.Pid)
+		// log.Println("PID:", cmd.Process.Pid)
 		cmd.Stdout = os.Stdout
 	}
 
@@ -67,38 +56,50 @@ func main() {
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, os.Interrupt)
 
-	log.Println("Start")
+	// log.Println("Start")
 	go func() {
 		<-sig
-		log.Println("Interrupt")
-		log.Println("Send signal to children")
+		// log.Println("Interrupt")
+		// log.Println("Send signal to children")
 
 		for _, cmd := range cmds {
 			if err := cmd.Process.Signal(os.Interrupt); err != nil {
-				log.Printf("error while killing child processes: %s", err)
+				// log.Printf("error while killing child processes: %s", err)
 				return
 			}
 
-			stat, err := cmd.Process.Wait()
+			_, err := cmd.Process.Wait()
 			if err != nil {
-				log.Printf("error while waiting child process: %s", err)
+				// log.Printf("error while waiting child process: %s", err)
 			}
 
-			log.Println("exit on ", stat.String())
+			// log.Println("exit on ", stat.String())
 		}
 
-		log.Println("process killed")
+		// log.Println("process killed")
 
 		exitChan <- struct{}{}
 	}()
 
 	go func() {
 		wg.Wait()
-		log.Println("processes finished")
+		// log.Println("processes finished")
 		exitChan <- struct{}{}
 	}()
 
 	<-exitChan
 
-	log.Println("Stop graceful")
+	// log.Println("Stop graceful")
+}
+
+func scan(wg *sync.WaitGroup, reader io.Reader, out io.Writer) {
+	defer func() {
+		// log.Println("close scan")
+	}()
+	// log.Println("create scanner")
+	s := bufio.NewScanner(reader)
+	for s.Scan() {
+		fmt.Fprintln(out, s.Text())
+	}
+	wg.Done()
 }
